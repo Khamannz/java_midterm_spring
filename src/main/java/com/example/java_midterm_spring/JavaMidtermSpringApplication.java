413716af287f69bc48178acb0bad08df5bba58d4
Kha Mann <Khamann@gmail.com>
2024-06-19 20:18:56 +0700

    first

---
+++ src/main/java/com/example/java_midterm_spring/JavaMidtermSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_midterm_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMidtermSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMidtermSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_midterm_spring/JavaMidtermSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_midterm_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMidtermSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
